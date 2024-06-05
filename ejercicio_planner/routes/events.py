dc2af752cb26ff4a5fcfaa96207d2dff23045139
SantiagusOK <santiagus315@gmail.com>
2024-06-04 21:26:42 -0300

    comiteo10

--- ejercicio_planner/routes/events.py
+++ ejercicio_planner/routes/events.py
@@ -1,7 +1,5 @@
-from typing import List
-
 from fastapi import APIRouter, HTTPException, status, Depends
-from sqlmodel import Session, select
+from sqlmodel import select
 from models.events import Event, EventBody
 from database.connect import get_session
 
@@ -21,20 +19,42 @@ events = []
 
 
 @event_router.get("/")
-async def retrieve_all_events(session:Session=Depends(get_session)):
-    statemts = select(Event)
+async def retrieve_all_events(session=Depends(get_session)):
+    statemts = select(Event).offset(4).limit(3).where(Event.title == "Gaturro")
     events = session.exec(statemts).all()
-    print(f"Valor del statemns------------------------------------------- {statemts}")
     return events
+
+@event_router.put("/update/{id}")
+async def update_a_event(id:int, session=Depends(get_session)):
+    
+    statements = select(Event).where(Event.id == id)
+    
+    eventUpdate = session.exec(statements)
+    
+    print(eventUpdate)
+    
+    eventUpdate.title = "Sanguchito"
+    
+    session.add(eventUpdate)
+    
+    session.commit()
+    
+    session.refresh(eventUpdate)
+    
+    raise HTTPException(
+        status.HTTP_200_OK,
+        detail="El titulo del evento se actualizo"
+    )
+    
 
 
 @event_router.get("/{id}", response_model=Event)
-async def retrieve_event(id: int, session: Session = Depends(get_session)) -> Event:
+async def retrieve_event(id: int, session= Depends(get_session)) -> Event:
+    
     event = session.get(Event, id)
 
     if event:
         return event
-    
     else:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -43,9 +63,12 @@ async def retrieve_event(id: int, session: Session = Depends(get_session)) -> Ev
 
 
 @event_router.post("/new")
-async def create_event(body: Event, session:Session=Depends(get_session)) -> dict:
+async def create_event(body: Event, session=Depends(get_session)) -> dict:
     session.add(body)
+    #print(f"Antes del refresh ID:{body.id}")
     session.commit()
+    
+    #print(f"Despues del refresh ID:{body.id}")
     session.refresh(body)
     return {
         "message": "Event created successfully"
@@ -53,11 +76,13 @@ async def create_event(body: Event, session:Session=Depends(get_session)) -> dic
 
 
 @event_router.delete("/delete/{id}")
-async def delete_event(id: int, session: Session = Depends(get_session)) -> dict:
+async def delete_event(id: int, session=Depends(get_session)) -> dict:
+    
     event = session.get(Event, id)
     
     if event:
-        session.delete()
+        session.delete(event)
+        session.commit()
         return {
             "message": "Event deleted successfully"
         }
--- flutter_proyects/flutter_application_1/lib/presentacion/pantalla_agregar_usuario.dart
+++ flutter_proyects/flutter_application_1/lib/presentacion/pantalla_agregar_usuario.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Dominio/data/adaptadores/adaptador_biblioteca.dart';
 import 'package:flutter_application_1/Dominio/entidades/usuario.dart';
